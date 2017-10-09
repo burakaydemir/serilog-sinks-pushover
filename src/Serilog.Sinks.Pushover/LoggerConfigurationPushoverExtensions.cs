1fc54fecfe088b4a17b8b28c10fc5bdf03272299
burak.aydemir <burak.aydemir@gmail.com>
2017-10-09 16:57:50 +0300

    Add base configuration and sink enhancements

---
+++ src/Serilog.Sinks.Pushover/LoggerConfigurationPushoverExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Serilog.Sinks.Pushover.Sinks.Pushover;
+
+namespace Serilog.Sinks.Pushover
+{
+    public static class LoggerConfigurationPushoverExtensions
+    {
+        const string DefaultPushoverUri = "https://api.pushover.net/1/messages.json";
+        const string DefaultPushoverOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+        public static LoggerConfiguration PushoverSink(
+            this LoggerSinkConfiguration loggerSinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            string outputTemplate = DefaultPushoverOutputTemplate,
+            string apiUri = DefaultPushoverUri,
+            string token = null,
+            string userOrGroupKey = null,
+            string[] devices = null,
+            IFormatProvider formatProvider = null,
+            LoggingLevelSwitch levelSwitch = null)
+        {
+            if (loggerSinkConfiguration == null) throw new ArgumentNullException(nameof(loggerSinkConfiguration));
+            if (token == null) throw new ArgumentNullException(nameof(token));
+            if (userOrGroupKey == null) throw new ArgumentNullException(nameof(userOrGroupKey));
+            if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+
+            return loggerSinkConfiguration.Sink(new PushoverSink(formatter, apiUri, token, userOrGroupKey, devices, restrictedToMinimumLevel, levelSwitch));
+        }
+    }
+}
---
+++ src/Serilog.Sinks.Pushover/Sinks/Pushover/PushoverSink.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace Serilog.Sinks.Pushover.Sinks.Pushover
+{
+    public class PushoverSink : ILogEventSink
+    {
+        private readonly ITextFormatter _textFormatter;
+        private readonly string _apiUri;
+        private readonly string _token;
+        private readonly string _userOrGroupKey;
+        private readonly string[] _devices;
+        private readonly LogEventLevel _logEventLevel;
+        private readonly LoggingLevelSwitch _loggingLevelSwitch;
+
+        public PushoverSink(ITextFormatter textFormatter, string apiUri, string token, string userOrGroupKey, string[] devices, LogEventLevel logEventLevel, LoggingLevelSwitch loggingLevelSwitch)
+        {
+            _textFormatter = textFormatter;
+            _apiUri = apiUri;
+            _token = token;
+            _userOrGroupKey = userOrGroupKey;
+            _devices = devices;
+            _logEventLevel = logEventLevel;
+            _loggingLevelSwitch = loggingLevelSwitch;
+        }
+
+        /// <summary>
+        /// Emit the provided log event to the sink.
+        /// </summary>
+        /// <param name="logEvent">The log event to write.</param>
+        public void Emit(LogEvent logEvent)
+        {
+            using (var buffer = new StringWriter())
+            {
+                _textFormatter.Format(logEvent, buffer);
+
+                try
+                {
+                    var parameters = new NameValueCollection {
+                        { "token", _token },
+                        { "title", "Something went wrong!" },
+                        { "user", _userOrGroupKey },
+                        { "message", buffer.ToString() },
+                        { "device", string.Join(",", _devices) }
+                    };
+
+                    byte[] response;
+                    using (var client = new WebClient())
+                    {
+                        response = client.UploadValues(_apiUri, parameters);
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+    }
+}
